e6d29f5215933ee9cfc902c585105a1dbc50d613
javython999 <infinity2544@gmail.com>
2023-08-03 22:43:32 +0900

    response test

--- src/main/java/com/example/myspringboot/HelloController.java
+++ src/main/java/com/example/myspringboot/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.myspringboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/example/myspringboot/MySpringBootApplication.java
@@ -1,37 +1,30 @@
 package com.example.myspringboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
 public class MySpringBootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet () {
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext
-                            .addServlet("dispatcherServlet", new DispatcherServlet(this))
-                            .addMapping("/*");
-                });
-                webServer.start();
-
-            }
-        };
-        applicationContext.register(MySpringBootApplication.class);
-        applicationContext.refresh();
-
-
+        SpringApplication.run(MySpringBootApplication.class, args);
     }
+
+
 
 }
