e6a35efd1d668440a6c06f72e873af19c6d90b49
javython999 <infinity2544@gmail.com>
2023-08-05 22:49:49 +0900

    meta annotation

--- src/test/java/com/example/myspringboot/HelloServiceTest.java
+++ src/test/java/com/example/myspringboot/HelloServiceTest.java
@@ -4,12 +4,34 @@ package com.example.myspringboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest { }
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void SimpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
         String ret = helloService.sayHello("Test");
         Assertions.assertThat(ret ).isEqualTo("Hello Test");
+    }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+        String ret = decorator.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("*Test*");
     }
 }
