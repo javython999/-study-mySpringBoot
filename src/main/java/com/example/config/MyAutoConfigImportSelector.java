cf955195e9219f2bd8a4743ba651a546575eb00a
javython999 <infinity2544@gmail.com>
2023-08-06 03:27:16 +0900

    AutoConfiguration imports

--- src/main/java/com/example/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/example/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,26 @@
 package com.example.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.example.config.autoconfig.DispatcherServletConfiguration",
-                "com.example.config.autoconfig.TomcatWebServerConfiguration",
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader)
+                .forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
