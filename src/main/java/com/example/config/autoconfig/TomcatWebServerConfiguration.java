f925d3848315f4939b2959edd1f936bff3223e0b
javython999 <infinity2544@gmail.com>
2023-08-10 21:29:59 +0900

    @Value, PropertyPlaceholderConfigurer

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfiguration.java
@@ -2,6 +2,7 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -10,9 +11,14 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfiguration {
+
+    @Value("${contextPath}")
+    String contextPath;
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(this.contextPath);
+        return factory;
     }
 }
